488208ece9abafceff4721b0425cd01d7932a15f
Ahn, Ian Tai <ian.tai.ahn@lmco.com>
2025-06-11 08:50:34 -0600

    removed the ssl flag since everything is secure

--- scraper.py
+++ scraper.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape_url(url):
-    response = requests.get(url, verify=False) #TODO Remove this verify=False in production
+    # response = requests.get(url, verify=False) #TODO Remove this verify=False in production
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     [s.extract() for s in soup(['script', 'style'])]
     text = ' '.join(p.get_text() for p in soup.find_all(['p', 'article', 'main']))
