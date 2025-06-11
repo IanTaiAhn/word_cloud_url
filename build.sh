3e77dbf885091e719443ce5543d3917309b82a11
Ahn, Ian Tai <ian.tai.ahn@lmco.com>
2025-06-11 12:56:58 -0600

    selected stable version

--- build.sh
+++ build.sh
@@ -20,17 +20,32 @@ fi
 # Install ChromeDriver if not already cached
 if [[ ! -d "$CHROMEDRIVER_DIR" ]]; then
   echo "🔽 Downloading ChromeDriver"
-  mkdir -p "$CHROMEDRIVER_DIR"
+#   mkdir -p "$CHROMEDRIVER_DIR"
 
-  CHROME_VERSION=$("$CHROME_DIR/opt/google/chrome/google-chrome" --version | grep -oP '\d+' | head -1)
-  echo "➤ Chrome version detected: $CHROME_VERSION"
+#   CHROME_VERSION=$("$CHROME_DIR/opt/google/chrome/google-chrome" --version | grep -oP '\d+' | head -1)
+#   echo "➤ Chrome version detected: $CHROME_VERSION"
 
-  CHROMEDRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-  echo "➤ ChromeDriver version to download: $CHROMEDRIVER_VERSION"
+#   CHROMEDRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
 
-  wget -O chromedriver.zip "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
+#   wget -O chromedriver.zip "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
+#   unzip chromedriver.zip -d "$CHROMEDRIVER_DIR"
+#   rm chromedriver.zip
+#   echo "➤ ChromeDriver version to download: $CHROMEDRIVER_VERSION"
+
+  #Pinned Chrome and Driver version
+  CHROME_VERSION=114.0.5735.90
+  
+  # Install Chrome
+  wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}-1_amd64.deb
+  dpkg -x google-chrome-stable_${CHROME_VERSION}-1_amd64.deb "$CHROME_DIR"
+  rm google-chrome-stable_${CHROME_VERSION}-1_amd64.deb
+  echo "➤ Chrome version installed: $CHROME_VERSION"
+
+  # Download matching ChromeDriver
+  wget -O chromedriver.zip "https://chromedriver.storage.googleapis.com/${CHROME_VERSION}/chromedriver_linux64.zip"
   unzip chromedriver.zip -d "$CHROMEDRIVER_DIR"
   rm chromedriver.zip
+  echo "➤ Chromedriver matched version installed: $CHROME_VERSION"
 else
   echo "✅ Using cached ChromeDriver"
 fi
